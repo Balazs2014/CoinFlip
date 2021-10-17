551ff28b16a9223d1dfd7b440ec2d045714716ec
Takács Balázs <tbalazs.levente@gmail.com>
2021-10-17 15:43:44 +0200

    Alert dialog 5 dobás után (új játék kezdhető), fejVagyIras metódus javítása

--- app/src/main/java/com/example/coinflip/MainActivity.java
+++ app/src/main/java/com/example/coinflip/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.coinflip;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -14,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     private ImageView kepErme;
     private Button gombFej, gombIras;
     private TextView textDobasok, textGyozelem, textVereseg;
-    private Random rnd;
     private int dobasokSzama, gyozelemSzama, veresegSzama;
     private String gepTipp, jatekosTipp;
 
@@ -29,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
             if (dobasokSzama != 5) {
                 jatekosTipp = "fej";
                 kiir(eredmeny(jatekosTipp, fejVagyIras()));
-                dobasokSzama++;
             }
         });
 
@@ -37,20 +38,26 @@ public class MainActivity extends AppCompatActivity {
             if (dobasokSzama != 5) {
                 jatekosTipp = "iras";
                 kiir(eredmeny(jatekosTipp, fejVagyIras()));
-                dobasokSzama++;
             }
         });
     }
 
     public String fejVagyIras() {
-        boolean random = rnd.nextBoolean();
+        dobasokSzama++;
+        double random = Math.floor(Math.random() * 2);
 
-        if (random) {
+        if (random == 1) {
             kepErme.setImageResource(R.drawable.heads);
             gepTipp = "fej";
+            Toast.makeText(MainActivity.this, "Fej", Toast.LENGTH_SHORT).show();
         } else {
             kepErme.setImageResource(R.drawable.tails);
             gepTipp = "iras";
+            Toast.makeText(MainActivity.this, "Írás", Toast.LENGTH_SHORT).show();
+        }
+
+        if (dobasokSzama == 5) {
+            jatekVege();
         }
 
         return gepTipp;
@@ -77,6 +84,44 @@ public class MainActivity extends AppCompatActivity {
         textDobasok.setText("Dobások: " + dobasokSzama);
         textGyozelem.setText("Győzelem: " + gyozelemSzama);
         textVereseg.setText("Vereség: " + veresegSzama);
+
+        if (gyozelemSzama == 3 || veresegSzama == 3) {
+            jatekVege();
+        }
+    }
+
+    public void jatekVege() {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+        alertBuilder.setCancelable(false);
+        if (gyozelemSzama >= 3) {
+            alertBuilder.setTitle("Győzelem");
+        } else {
+            alertBuilder.setTitle("Vereség");
+        }
+        alertBuilder.setMessage("Szeretnél új játékot játszani?");
+        alertBuilder.setPositiveButton("Igen", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ujJatek();
+            }
+        });
+
+        alertBuilder.setNegativeButton("Nem", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        alertBuilder.create().show();
+    }
+
+    public void ujJatek() {
+        dobasokSzama = 0;
+        gyozelemSzama = 0;
+        veresegSzama = 0;
+        textDobasok.setText("Dobások: 0");
+        textGyozelem.setText("Győzelem: 0");
+        textVereseg.setText("Vereség: 0");
     }
 
     public void init() {
